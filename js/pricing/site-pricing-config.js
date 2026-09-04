2ba18ec42974726081164b6de8461a89131e8652
Nafeson <rightglaze@gmail.com>
2026-09-04 23:37:27 +0100

    Update site-pricing-config.js

--- js/pricing/site-pricing-config.js
+++ js/pricing/site-pricing-config.js
@@ -59,7 +59,7 @@ const ALLOWED_STRATEGIES = Object.freeze([
 * =========================================
 */
 
-const ACTIVE_STRATEGY = "cheap";
+const ACTIVE_STRATEGY = "normal";
 
 function normalizeStrategy(strategy) {
 const value = String(strategy ?? "")
